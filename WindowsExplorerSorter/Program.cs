df117db5d220578e30177828e09e27593c2b9dca
GERJanB <jan.budde.jb@gmail.com>
2022-08-12 13:41:51 +0200

    Initial Commit

---
+++ WindowsExplorerSorter/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WindowsExplorerSorter {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
