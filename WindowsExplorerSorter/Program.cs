a0dc6565768684fededf6466f706e72fba8371cf
GERJanB <jan.budde.jb@gmail.com>
2022-08-13 09:49:34 +0200

    Test Program

--- WindowsExplorerSorter/Program.cs
+++ WindowsExplorerSorter/Program.cs
@@ -3,7 +3,9 @@
 namespace WindowsExplorerSorter {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Sorter sorter = new();
+            var s = sorter.GetFileExtensions();
+            s.ForEach(x => Console.WriteLine(x));
         }
     }
 }
