3f3ce1884f9dcd74b0462168dbee02fb41079f98
GERJanB <jan.budde.jb@gmail.com>
2022-08-13 09:47:35 +0200

    Add Method to get FileExtensions)

--- WindowsExplorerSorter/Sorter.cs
+++ WindowsExplorerSorter/Sorter.cs
@@ -3,8 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WindowsExplorerSorter {
     public class Sorter {
+        private readonly string path = "C:\\Users\\janbu\\Desktop\\SorterTest";
+
+        public List<string> GetFileExtensions() {
+            List<string> extensions = new();
+            List<string> files = Directory.GetFiles(path).ToList();
+
+            foreach (string file in files) {
+                var s = Path.GetExtension(file);
+                if (!extensions.Contains(s)) {
+                    extensions.Add(s);
+                }
+            }
+
+            return extensions;
+        }
     }
 }
